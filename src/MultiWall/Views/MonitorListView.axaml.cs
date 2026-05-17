331aea51f1618e14690c55505d118aa7aa5e605e
wzx5002 <WZX@tongji.edu.cn>
2026-05-17 17:36:20 +0800

    fix context menu: use ContextRequested event for Win11 compatibility

--- src/MultiWall/Views/MonitorListView.axaml.cs
+++ src/MultiWall/Views/MonitorListView.axaml.cs
@@ -1,4 +1,8 @@
 using Avalonia.Controls;
+using Avalonia.Input;
+using MultiWall.Models;
+using MultiWall.Services;
+using MultiWall.ViewModels;
 
 namespace MultiWall.Views;
 
@@ -7,5 +11,26 @@ public partial class MonitorListView : UserControl
     public MonitorListView()
     {
         InitializeComponent();
+    }
+
+    private void OnContextRequested(object? sender, ContextRequestedEventArgs e)
+    {
+        if (sender is not Control control) return;
+        if (control.DataContext is not MonitorInfo monitor || !monitor.IsSlideshow) return;
+
+        var menu = new ContextMenu();
+        var item = new MenuItem
+        {
+            Header = LocalizationService.GetString("Label.NextWallpaper")
+        };
+        item.Click += (_, _) =>
+        {
+            if (VisualRoot is Window w && w.DataContext is MainWindowViewModel vm)
+                vm.NextWallpaperCommand.Execute(monitor);
+        };
+        menu.Items.Add(item);
+        menu.Open(control);
+
+        e.Handled = true;
     }
 }
